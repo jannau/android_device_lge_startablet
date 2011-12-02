cb5665196bb9e552b06b12ecd519d5c2e800d78e
Aaron Echols <atechols@gmail.com>
2011-12-02 12:50:46 -0700

    fixed menu misallignment

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -23,14 +23,12 @@
 char* MENU_HEADERS[] = { NULL };
 
 char* MENU_ITEMS[] = { "reboot system now",
-                       "apply update from sdcard",
+                       "install zip from sdcard",
                        "wipe data/factory reset",
                        "wipe cache partition",
-                       "install zip from sdcard",
                        "backup and restore",
                        "mounts and storage",
                        "advanced",
-                       "power off",
                        NULL };
 
 int device_recovery_start() {
